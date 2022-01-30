d35b7f1f72881808eab0c1b10f74ff74938730ae
Joaquin Abregu <abregujoaquin@gmail.com>
2022-01-29 22:03:29 -0300

    Election component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Nav } from "./components/Nav";
+import { Election } from "./screens/Election";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Election />
     </div>
   );
 }
