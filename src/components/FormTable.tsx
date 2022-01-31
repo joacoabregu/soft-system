1bba1bd02f0356695ec19e9af65f217e1283c41f
Joaquin Abregu <abregujoaquin@gmail.com>
2022-01-30 21:43:26 -0300

    added FormTableBody

--- src/components/FormTable.tsx
+++ src/components/FormTable.tsx
@@ -1,17 +1,13 @@
 /** @jsxImportSource @emotion/react */
 import React from "react";
 import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import DeleteIcon from "@mui/icons-material/Delete";
 import { FormTableProps } from "../types/interfaces";
+import FormTableBody from "./FormTableBody";
 
 export default function FormTable({
   columns,
@@ -19,57 +15,6 @@ export default function FormTable({
   resetField,
   register,
 }: FormTableProps) {
-  let resultRows = [];
-
-  for (let i = 0; i < rows; i++) {
-    const item = (
-      <TableRow
-        key={i}
-        sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-      >
-        <TableCell>
-          <TextField
-            {...register(`candidates.${i}.name`, { required: true })}
-            id="outlined-basic"
-            fullWidth
-            size="small"
-          />
-        </TableCell>
-        <TableCell>
-          <TextField
-            {...register(`candidates.${i}.description`, { required: true })}
-            id="outlined-basic"
-            fullWidth
-            size="small"
-          />
-        </TableCell>
-        <TableCell sx={{ display: "flex", alignItems: "center" }}>
-          <label htmlFor="contained-button-file">
-            <input
-              {...register(`candidates.${i}.image`)}
-              accept="image/*"
-              id="contained-button-file"
-              multiple
-              type="file"
-              css={{ display: "none" }}
-            />
-            <Button variant="contained" component="span" size="small">
-              Imagen
-            </Button>
-          </label>
-          <IconButton
-            aria-label="delete"
-            onClick={() => resetField(`candidates.${i}.image`)}
-          >
-            <DeleteIcon />
-          </IconButton>
-        </TableCell>
-      </TableRow>
-    );
-
-    resultRows.push(item);
-  }
-
   return (
     <TableContainer component={Paper} sx={{ marginBottom: "1em" }}>
       <Table>
@@ -84,7 +29,11 @@ export default function FormTable({
             })}
           </TableRow>
         </TableHead>
-        <TableBody>{resultRows}</TableBody>
+        <FormTableBody
+          rows={rows}
+          register={register}
+          resetField={resetField}
+        />
       </Table>
     </TableContainer>
   );
