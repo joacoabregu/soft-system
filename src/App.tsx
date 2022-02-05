a72e247a186124ab4ed05a4af0eee49a6a4a9859
Joaquin Abregu <abregujoaquin@gmail.com>
2022-02-05 10:37:25 -0300

    added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import "./App.css";
 import { Nav } from "./components/Nav";
 import { Election } from "./screens/Election";
 
 function App() {
   return (
-    <div className="App">
+    <div style={{ textAlign: "center", backgroundColor: "#dddddd" }}>
       <Nav />
       <Election />
     </div>
