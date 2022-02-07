bf39e92b3cc3b0ffa06a16f73f1a04474644ef49
Joaquin Abregu <abregujoaquin@gmail.com>
2022-02-07 17:21:39 -0300

    added create mode

--- src/components/ToggleColorMode.tsx
+++ src/components/ToggleColorMode.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import { ColorModeContext } from "../context/ColorModeContext";
 import { ToggleColorModeProps } from "../types/interfaces";
-import { theme } from "../styles/theme";
+import {  createMode  } from "../styles/theme";
 
 export default function ToggleColorMode({ children }: ToggleColorModeProps) {
   const [mode, setMode] = React.useState<"light" | "dark">("light");
@@ -15,18 +15,8 @@ export default function ToggleColorMode({ children }: ToggleColorModeProps) {
     []
   );
 
-  function createMode(mode: "light" | "dark") {
-    return {
-      ...theme,
-      palette: {
-        ...theme.palette,
-        mode,
-      },
-    };
-  }
-
   const themeMode = React.useMemo(() => createMode(mode), [mode]);
-
+ 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={themeMode}>{children}</ThemeProvider>
