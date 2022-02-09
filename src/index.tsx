81036b0b5f188a09c83bd26118f3befbd89680c1
Joaquin Abregu <abregujoaquin@gmail.com>
2022-02-09 17:35:19 -0300

    added theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import ToggleColorMode from "./components/ToggleColorMode";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ToggleColorMode>
+        <App />
+      </ToggleColorMode>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
