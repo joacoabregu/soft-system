f301a9310b8f8b9774be17671149b9bc92c4e008
Joaquin Abregu <abregujoaquin@gmail.com>
2022-02-09 17:35:37 -0300

    removed theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
 import React from "react";
+import useTheme from "@mui/material/styles/useTheme";
+import { Routes, Route } from "react-router-dom";
+
 import { Nav } from "./components/Nav";
 import { Election } from "./screens/Election";
-import ToggleColorMode from "./components/ToggleColorMode";
-import { Routes, Route,  } from "react-router-dom";
-import Voting from "./screens/Voting"
+//import ToggleColorMode from "./components/ToggleColorMode";
+import Voting from "./screens/Voting";
 
 function App() {
+  const {
+    palette: { background },
+  } = useTheme();
 
   return (
-    <ToggleColorMode>
-      <div
-        style={{
-          textAlign: "center",
-          backgroundColor: "#dddddd",
-          paddingBottom: "3em",
-        }}
-      >
-        <Nav />
-        <Routes>
-          <Route path="/voting" element={<Voting />} />
-          <Route path="/" element={<Election />} />
-        </Routes>
-      </div>
-    </ToggleColorMode>
+    <div
+      style={{
+        textAlign: "center",
+        backgroundColor: background.default,
+        paddingBottom: "3em",
+      }}
+    >
+      <Nav />
+      <Routes>
+        <Route path="/voting" element={<Voting />} />
+        <Route path="/" element={<Election />} />
+      </Routes>
+    </div>
   );
 }
 
