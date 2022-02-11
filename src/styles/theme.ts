7a4928b5daa2fbad79adbc597bade40eb6e70895
Joaquin Abregu <abregujoaquin@gmail.com>
2022-02-11 12:44:41 -0300

    dark mode

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,109 +1,125 @@
+import { PaletteMode } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 
 declare module "@mui/material/styles" {
   interface Theme {
     back: {
-      white: string;
+      main: string;
     };
   }
   interface ThemeOptions {
     back?: {
-      white?: string;
+      main?: string;
     };
   }
 }
 
-const theme = createTheme({
-  palette: {
-    primary: { main: "#545454" },
-
-    background: {
-      default: "#dddddd",
+const createMode = (mode: PaletteMode) => {
+  const theme = {
+    palette: {
+      mode,
+      primary: {
+        ...(mode === "light" ? { main: "#545454" } : { main: "#b3b3b3" }),
+      },
+      background: {
+        ...(mode === "light" ? { default: "#e7e7e7" } : { default: "#585757" }),
+      },
+      ...(mode === "dark" && {
+        text: {
+          primary: "#c6c5c6",
+        },
+      }),
     },
-  },
-  back: {
-    white: "#FFF",
-  },
-  typography: {
-    fontSize: 15,
-    h1: {
-      fontSize: "2.3rem",
-      fontWeight: 500,
+    back: {
+      ...(mode === "light" ? { main: "#FFF" } : { main: "#2b2b2b" }),
     },
-    h2: {
-      fontSize: "2rem",
+    typography: {
+      fontSize: 15,
+      h1: {
+        fontSize: "2.3rem",
+        fontWeight: 500,
+      },
+      h2: {
+        fontSize: "2rem",
+      },
+      h3: {
+        fontSize: "1.7rem",
+      },
+      fontFamily: [
+        "Poppins",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(","),
     },
-    h3: {
-      fontSize: "1.7rem",
-    },
-
-    fontFamily: [
-      "Poppins",
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
-  },
-  components: {
-    MuiOutlinedInput: {
-      styleOverrides: {
-        root: {
-          backgroundColor: "#f3f3f37d",
-          borderRadius: 16,
+    components: {
+      MuiOutlinedInput: {
+        styleOverrides: {
+          root: {
+            ...(mode === "light"
+              ? { backgroundColor: "#f3f3f37d" }
+              : { backgroundColor: "#1f1f1f" }),
+            borderRadius: 16,
+          },
         },
       },
-    },
-    MuiTextField: {
-      styleOverrides: {
-        root: {
-          width: "100%",
-          marginBottom: "2em",
+      MuiTextField: {
+        styleOverrides: {
+          root: {
+            width: "100%",
+            marginBottom: "2em",
+          },
         },
       },
-    },
-    MuiDivider: {
-      styleOverrides: {
-        root: {
-          margin: "1em 0 1.5em 0",
+      MuiDivider: {
+        styleOverrides: {
+          root: {
+            ...(mode === "dark" && {
+              borderColor: "#c6c5c6",
+            }),
+            margin: "1em 0 1.5em 0",
+          },
         },
       },
-    },
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          "&:hover": {
-            borderColor: "#545454",
-            backgroundColor: " #f3f3f37d",
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            "&:hover": {
+              borderColor: "#545454",
+              backgroundColor: " #f3f3f37d",
+            },
+          },
+        },
+      },
+      MuiButtonBase: {
+        styleOverrides: {
+          root: {
+            ...(mode === "dark" && {
+              color: "#545454",
+            }),
+          },
+        },
+      },
+      MuiAlert: {
+        styleOverrides: {
+          root: {
+            ...(mode === "dark" && {
+              backgroundColor: " #2b2b2b",
+            }),
           },
         },
       },
     },
-  },
-});
-
-const darkModeProps = {
-  back: { white: "#2b2b2b" },
+  };
+  const finalTheme = createTheme(theme);
+  return finalTheme;
 };
 
-function createMode(mode: "light" | "dark") {
-  return {
-    ...theme,
-    palette: {
-      ...theme.palette,
-      mode,
-    },
-    back: {
-      ...theme.back,
-      ...(mode === "dark" ? darkModeProps.back : null),
-    },
-  };
-}
-
-export { theme, createMode };
+export { createMode };
